30a54a88a44f40b9cb179d9097fd3eae9d0ac48e
Theconnoiser <cenitelll@gmail.com>
2023-10-31 20:05:02 +0200

    text with Loxa4

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseUrl = "http://kanban-app-server-eight.vercel.app/api/v1/";
+const baseUrl = "https://kanban-app-server-eight.vercel.app/api/v1/";
 const getToken = () => localStorage.getItem("token");
 
 const axiosClient = axios.create({
